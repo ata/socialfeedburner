3dfe0d804f6dbb5c35846fcbc453d9418ad7b03a
Ahmad Tanwir <th3crypt@gmail.com>
2010-12-24 05:34:37 +0700

    Mulai coding js

---
+++ twitterauth/models.py
@@ -0,0 +1,39 @@
+import re, httplib, django.utils.simplejson
+from django.db import models
+from oauth import oauth
+from utils import *
+
+class User(models.Model):
+    username = models.CharField(max_length=40)
+    email = models.EmailField()
+    oauth_token = models.CharField(max_length=200)
+    oauth_token_secret = models.CharField(max_length=200)
+
+    def validate(self):
+        errors = []
+        if self.username and not re.compile('^[a-zA-Z0-9_]{1,40}$').match( \
+            self.username):
+            errors += ['username']
+        if self.email and not re.compile( \
+            '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$').match( \
+            self.email):
+            errors += ['email']
+        return errors
+
+    # django.core.context_processors.auth assumes that an object attached
+    # to request.user is always a django.contrib.auth.models.User, which
+    # is completely broken but easy to work around
+    def get_and_delete_messages(self): pass
+
+    def token(self):
+        return oauth.OAuthToken(self.oauth_token, self.oauth_token_secret)
+
+    def is_authorized(self): return is_authorized(self.token())
+
+    def tweet(self, status):
+        return api(
+            'https://twitter.com/statuses/update.json',
+            self.token(),
+            http_method='POST',
+            status=status
+        )
