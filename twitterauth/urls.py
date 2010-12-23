6913ebbcdef3788d7098f30a31f1c5a8db561859
Ahmad Tanwir <th3crypt@gmail.com>
2010-12-24 05:35:40 +0700

    Mulai coding js

--- twitterauth/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls.defaults import patterns, url
-
-
-urlpatterns = patterns('twitterauth.views',
-    url(r'^info/?$', 'info', name='twitterauth_info'),
-    url(r'^login/?$', 'login', name='twitterauth_login'),
-    url(r'^login/callback/?$', 'callback', name='twitterauth_callback'),
-    url(r'^logout/?$', 'logout', name='twitterauth_logout'),
-)
