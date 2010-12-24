bc58647be53fee9bc9bc717b41f1fa03b0f691f8
Ahmad Tanwir <th3crypt@gmail.com>
2010-12-24 08:34:17 +0700

    Bingung di oAuth

--- socialauth/views.py
+++ socialauth/views.py
@@ -24,23 +24,27 @@ def profile(request):
     return render_to_response('socialauth/profile.html', {'form': form ,})
 
 def twitter_connect(request):
-    token = twitter.get_unauthorized_token()
+    try:
+       token = twitter.get_unauthorized_token()
+    except:
+        return render_to_response('socialauth/twitter_connect_error.html')
     request.session['token'] = token.to_string()
     return HttpResponseRedirect(twitter.get_authorization_url(token))
+    
 
 def twitter_connect_done(request):
     token = request.session.get('token', None)
     if not token:
-        return render_to_response('callback.html', {'token': True})
+        return render_to_response('socialauth/twitter_connect_error.html', {'token': True})
     token = oauth.OAuthToken.from_string(token)
     if token.key != request.GET.get('oauth_token', 'no-token'):
-        return render_to_response('socialauth/twitter_connect_done.html', {'mismatch': True})
+        return render_to_response('socialauth/twitter_connect_error.html', {'mismatch': True})
     token = twitter.get_authorized_token(token)
 
     # Actually login
     obj = twitter.is_authorized(token)
     if obj is None:
-        return render_to_response('socialauth/twitter_connect_done.html', {'username': True})
+        return render_to_response('socialauth/twitter_connect_error.html', {'username': True})
     try: 
         twitter_profile = TwitterUserProfile.objects.get(\
                   screen_name=obj['screen_name'])
